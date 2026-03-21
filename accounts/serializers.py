4a66afa0921c53477289a1dcbefed59a52570db7
asishp-affinsys <asish.patro@affinsys.com>
2026-03-21 11:07:59 +0530

    added api endpoints for user account operations

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -18,3 +18,25 @@ class BootstrapAdminResponseSerializer(serializers.Serializer):
 
 class SetupStatusResponseSerializer(serializers.Serializer):
     admin_exists = serializers.BooleanField()
+
+class CreateAccountRequestSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True, min_length=6)
+    role = serializers.ChoiceField(choices=["admin", "developer", "viewer"])
+
+class CreateAccountResponseSerializer(serializers.Serializer):
+    message = serializers.CharField()
+    user = serializers.DictField(child=serializers.CharField())
+
+class UserListResponseSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    username = serializers.CharField()
+    is_active = serializers.BooleanField()
+    role = serializers.CharField()
+
+class UserStatusRequestSerializer(serializers.Serializer):
+    user_id = serializers.IntegerField()
+
+class MessageResponseSerializer(serializers.Serializer):
+    message = serializers.CharField()
+    
